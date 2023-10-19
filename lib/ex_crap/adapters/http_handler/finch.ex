ed5a288263917644a6a1d80cadc365cf576836e5
Elton Campos <eltoncampos36@gmail.com>
2023-10-19 19:17:04 -0300

    feat: create http handler

---
+++ lib/ex_crap/adapters/http_handler/finch.ex
@@ -0,0 +1,10 @@
+defmodule Core.Adapters.HttpHandler.Finch do
+  @behaviour Core.Ports.HttpHandler
+
+  def get(url) do
+   case  Finch.build(:get, url) |> Finch.request(:ex_finch) do
+    {:ok, %Finch.Response{body: body}} -> {:ok, body}
+    {:error, _reason} = err -> err
+   end
+  end
+end

c467ef2772fb7803cddf7debd071c41e69ca8425
Elton Campos <eltoncampos36@gmail.com>
2023-10-20 00:52:30 -0300

    fix: migration

--- priv/repo/migrations/20231019203143_create_links_table.exs
+++ priv/repo/migrations/20231019203143_create_links_table.exs
@@ -6,7 +6,8 @@ defmodule Core.Repo.Migrations.CreateLinksTable do
       add :id, :binary_id, primary_key: true
       add :content, :text
       add :href, :string
-      add :user_id, references(:pages, on_delete: :delete_all, type: :binary_id), null: false
+      add :page_id, references(:pages, on_delete: :delete_all, type: :binary_id), null: false
+
       timestamps()
     end
   end
